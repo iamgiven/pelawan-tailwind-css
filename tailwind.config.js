409b75eb1dfe11df8f9a65ae82cff93a18d382de
iamgiven <givendra03@gmail.com>
2022-06-12 15:39:46 +0700

    project pelawan

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        theme: ['Luckiest Guy'],
+        title: ['Merriweather'],
+      },
+    },
+  },
+  plugins: [],
+}
